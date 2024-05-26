e0f6c1de5886c9a4539a944c7358e7d152f3dbb5
Joe Anderson <joseph.anderson423@gmail.com>
2024-05-26 11:37:04 -0600

    adds custom styles and header/footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <h1>Seven Boughs</h1>
-  )
-}
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Seven Boughs</h1>
+        </Container>
+        <Footer />
+      </main>
+    </>
+  );
+};
 
-export default App
+export default App;
