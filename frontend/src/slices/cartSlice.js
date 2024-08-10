d3fefe52e682771c88ad784d06c5917ad55627af
Joe Anderson <joseph.anderson423@gmail.com>
2024-08-10 17:18:01 -0600

    adds shopping cart functionality

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -5,7 +5,6 @@ const initialState = localStorage.getItem("cart")
   ? JSON.parse(localStorage.getItem("cart"))
   : { cartItems: [] };
 
-
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -16,16 +15,23 @@ const cartSlice = createSlice({
       const existItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existItem) {
-        state.cartItems = state.cartItems.map((x) => x._id === existItem._id ? item : x )
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
       } else {
         state.cartItems = [...state.cartItems, item];
       }
 
       return updateCart(state);
     },
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
+
+      return updateCart(state);
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
